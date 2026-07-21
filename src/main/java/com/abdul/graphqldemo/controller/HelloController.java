6b44f263bbbaeb02b1905a665e924308ad5bb40a
unknown <abdulrazak.mca@gmail.com>
2026-07-21 19:52:04 +0530

    Add employeeById GraphQL query with argument

--- src/main/java/com/abdul/graphqldemo/controller/HelloController.java
+++ src/main/java/com/abdul/graphqldemo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.abdul.graphqldemo.controller;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -33,8 +34,25 @@ public class HelloController {
 	
 	@QueryMapping
 	public List<Employee> employees(){
+		return getEmployees();
+	}
+	
+	@QueryMapping
+	public Employee employeeById(@Argument Long id) {
+	   return getEmployees().stream()
+				            .filter(byId(id))
+				            .findFirst()
+				            .orElse(null);
+	}
+	
+	private List<Employee> getEmployees(){
 		Employee e1 = new Employee(1L, "Abdul", 10000.50);
 		Employee e2 = new Employee(2L, "Razak", 20000.50);
 		return List.of(e1,e2);
+	}
+	
+	private static Predicate<Employee> byId(Long id){
+		
+		return emp -> emp.getId().equals(id);
 	}
 }
