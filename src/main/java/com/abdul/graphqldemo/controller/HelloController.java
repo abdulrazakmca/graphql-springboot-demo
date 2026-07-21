402c63228dd89b31713cd38129cb4d2aca784b63
unknown <abdulrazak.mca@gmail.com>
2026-07-21 19:03:20 +0530

    Add GraphQL query returning list of employees

--- src/main/java/com/abdul/graphqldemo/controller/HelloController.java
+++ src/main/java/com/abdul/graphqldemo/controller/HelloController.java
@@ -1,8 +1,12 @@
 package com.abdul.graphqldemo.controller;
 
+import java.util.List;
+
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import com.abdul.graphqldemo.model.Employee;
 
 @Controller
 public class HelloController {
@@ -12,14 +16,25 @@ public class HelloController {
 //	        return "Hello GraphQL";
 //	    }
 //	  
-	    @QueryMapping
-	    public String hello(@Argument String name) {
+	@QueryMapping
+	public String hello(@Argument String name) {
 
-	        if (name == null || name.isBlank()) {
-	            return "Hello Guest";
-	        }
+		if (name == null || name.isBlank()) {
+			return "Hello Guest";
+		}
 
-	        return "Hello " + name;
-	    }
-	    
+		return "Hello " + name;
+	}
+
+	@QueryMapping
+	public Employee employee() {
+		return new Employee(1L, "Abdul", 10000.50);
+	}
+	
+	@QueryMapping
+	public List<Employee> employees(){
+		Employee e1 = new Employee(1L, "Abdul", 10000.50);
+		Employee e2 = new Employee(2L, "Razak", 20000.50);
+		return List.of(e1,e2);
+	}
 }
