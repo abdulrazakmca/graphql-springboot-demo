209ec4162d3ecd2cb71ae9f295909f3047bf1f36
unknown <abdulrazak.mca@gmail.com>
2026-07-21 18:44:25 +0530

    Add GraphQL query with argument

--- src/main/java/com/abdul/graphqldemo/controller/HelloController.java
+++ src/main/java/com/abdul/graphqldemo/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.abdul.graphqldemo.controller;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HelloController {
 
-	  @QueryMapping
-	  public String hello() {
-	        return "Hello GraphQL";
+//	  @QueryMapping
+//	  public String hello() {
+//	        return "Hello GraphQL";
+//	    }
+//	  
+	    @QueryMapping
+	    public String hello(@Argument String name) {
+
+	        if (name == null || name.isBlank()) {
+	            return "Hello Guest";
+	        }
+
+	        return "Hello " + name;
 	    }
+	    
 }
